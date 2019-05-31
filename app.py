082fa8ab050dc1104424483de720cfcd722d5dd3
dongwei <dongwei@gstianfu.com>
2019-05-31 17:08:36 +0800

    update app

--- app.py
+++ app.py
@@ -3,12 +3,19 @@ import gzip
 import time
 import conf
 import json
+import datetime
 import requests
 import functools
 from cache_util import cache
 from websocket import create_connection
+from sms import aliyun_send
 
+phone = '17701225865'
 send_key = '12431-0355c73a8b4ddbf191cce09acdccbfbc'
+
+
+def write_log(message):
+    print('%s: %s' % (datetime.datetime.now(), message))
 
 
 def time_lock(func):
@@ -23,26 +30,28 @@ def time_lock(func):
 
 
 @time_lock
-def send_notify(text):
+def send_notify(action, price):
+    text = 'ETH已%s至指定价格%s，请及时关注。' % (action, price)
     notify_url = f'https://pushbear.ftqq.com/sub?sendkey={send_key}&text={text}'
-    print('#notify: ', notify_url)
+    write_log('#notify: %s' % notify_url)
     requests.get(notify_url)
+    aliyun_send(phone, action, str(price))
 
 
 def check_eth_price(price):
 
     def _check_min_value(value):
         if price <= float(value):
-            send_notify('ETH已下跌至指定价格%s，请及时关注。' % price)
+            send_notify('下跌', price)
 
     def _check_max_value(value):
         if price >= float(value):
-            send_notify('ETH已上涨至指定价格%s，请及时关注。' % price)
+            send_notify('上涨', price)
     try:
         list(map(_check_min_value, str(conf.MIN_VALUE).split('|')))
         list(map(_check_max_value, str(conf.MAX_VALUE).split('|')))
     except Exception as ex:
-        print(ex)
+        write_log(ex)
 
 
 if __name__ == '__main__':
@@ -51,7 +60,7 @@ if __name__ == '__main__':
             ws = create_connection("wss://api.huobi.pro/ws")
             break
         except:
-            print('connect ws error,retry...')
+            write_log('connect ws error,retry...')
             time.sleep(5)
 
     # 订阅 KLine 数据
@@ -70,7 +79,7 @@ if __name__ == '__main__':
             else:
                 data = json.loads(result)
                 if 'tick' in data:
-                    print(data)
+                    write_log(data)
                     check_eth_price(float(data['tick']['close']))
         except Exception:
             time.sleep(5)
--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 from rtconfig.client import RtConfigClient
 
 client = RtConfigClient('market',
-                        ws_url='ws://127.0.0.1:5015',
+                        ws_url='ws://127.0.0.1',
                         config_module=globals())
